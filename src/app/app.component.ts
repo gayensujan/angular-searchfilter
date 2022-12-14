9d25f39b42fa7af237bf89733274a0ab723f7185
unknown <gayensujan1996@gmail.com>
2022-12-14 14:38:06 +0530

    sample commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,68 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'search-data';
+  filterMember!: string;
+  userRecords = [
+    {
+      id: 1,
+      name: 'Neanne Graham',
+      username: 'Bret',
+      email: 'Sincere@gmail.com',
+    },
+    {
+      id: 2,
+      name: 'Rrvin Howell',
+      username: 'Antonette',
+      email: 'Shanna@gmail.com',
+    },
+    {
+      id: 3,
+      name: 'Dlementine Bauch',
+      username: 'Samantha',
+      email: 'Nathan@gmail.com',
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      username: 'Karianne',
+      email: 'Julianne.OConner@gmail.com',
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@gmail.com',
+    },
+    {
+      id: 6,
+      name: 'Mrs. Fennis Schulist',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@gmail.com',
+    },
+    {
+      id: 7,
+      name: 'Gurtis Weissnat',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@gmail.com',
+    },
+    {
+      id: 8,
+      name: 'Nicholas Runolfsdottir ',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@gmail.com',
+    },
+    {
+      id: 9,
+      name: 'Glenna Reichert',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@gmail.com',
+    },
+    {
+      id: 10,
+      name: 'Clementina DuBuque',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@gmail.com',
+    },
+  ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Ng2SearchPipeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
